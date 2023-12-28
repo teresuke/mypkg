bb3783982afcf2e4ed26afe1b4c05ac231bc504c
teresuke <shunsuke 0127 teresa heiho->
2023-12-28 22:15:44 +0900

    Add a test2

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,18 +1,29 @@
-import rclpy                     #ROS 2のクライアントのためのライブラリ
-from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
+#!/usr/bin/python3
+# SPDX=FileCopyrightText: 2023 Shunsuke Otani
+# SPDX-License-Identifier: BSD-3-Clause
 
-from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
+import rclpy                     
+from rclpy.node import Node      
+from std_msgs.msg import Int16   
 
-rclpy.init()
-node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
-pub = node.create_publisher(Int16, "countup", 10)   #パブリッシャのオブジェクト作成
-n = 0 #カウント用変数
-def cb():          #17行目で定期実行されるコールバック関数
-    global n       #関数を抜けてもnがリセットされないようにしている
-    msg = Int16()  #メッセージの「オブジェクト」
-    msg.data = n   #msgオブジェクトの持つdataにnを結び付け
-    pub.publish(msg)        #pubの持つpublishでメッセージ送信
-    n += 1
-                       
-node.create_timer(0.5, cb)   #タイマー設定
-rclpy.spin(node)    
+class Talker():
+    def __init__(self, node):
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        node.create_timer(0.5, self.cb) 
+
+    def cb(self):      
+        msg = Int16()
+        msg.data = self.n     
+        self.pub.publish(msg) 
+        self.n += 1
+
+
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
